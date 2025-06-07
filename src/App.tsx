6b9209606c15ebfc56977bf0cec4b10f6be77519
Abhishek Baddi <abybaddi009@gmail.com>
2025-06-07 14:11:32 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">Hello world!</h1>
+      <Button>Click me</Button>
+    </div>
+  );
+}
+
+export default App;
